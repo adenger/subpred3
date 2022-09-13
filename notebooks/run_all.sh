c27bf5bbf2ed3896769fd9d21b01c34ece386c44
Andreas Denger <a.denger@outlook.de>
2022-09-13 13:14:32 +0200

    recursive script

--- notebooks/run_all.sh
+++ notebooks/run_all.sh
@@ -2,4 +2,5 @@
 
 # Make sure that conda env is activated, and screen session is started.
 
-for nb in *.ipynb ; do jupyter nbconvert --to notebook --inplace --execute $nb ; done
+# for nb in *.ipynb ; do jupyter nbconvert --to notebook --inplace --execute $nb ; done
+for nb in $(find -name '*.ipynb' | sort); do jupyter nbconvert --to notebook --inplace --execute $nb ; done
